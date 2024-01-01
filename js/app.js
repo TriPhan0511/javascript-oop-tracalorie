74e1a01f5c7ef1d3083460145517760f79eb51e2
Tri Phan <phanphutri@gmail.com>
2024-01-01 18:02:27 +0700

    Updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// CalorieTracker class
 class CalorieTracker {
   constructor() {
     this._calorieLimit = 2000
@@ -15,8 +16,151 @@ class CalorieTracker {
     this._workouts.push(workout)
     this._totalCalories -= workout.calories
   }
+
+  _displayCaloriesTotal(ele) {
+    if (ele) {
+      ele.textContent = this.caloriesTotal
+    }
+  }
+
+  _displayCaloriesLimit(ele) {
+    if (ele) {
+      ele.textContent = this._calorieLimit
+    }
+  }
+
+  _displayCaloriesConsumed(ele) {
+    if (ele) {
+      ele.textContent = this.caloriesConsumed
+    }
+  }
+
+  _displayCaloriesBurned(ele) {
+    if (ele) {
+      ele.textContent = this.caloriesBurned
+    }
+  }
+
+  _displayCaloriesRemaining(ele) {
+    if (ele) {
+      ele.textContent = this.caloriesRemaining
+    }
+  }
+
+  _renderStats() {
+    const limitDiv = document.querySelector('#calories-limit')
+    const totalDiv = document.querySelector('#calories-total')
+    const consumedDiv = document.querySelector('#calories-consumed')
+    const burnedDiv = document.querySelector('#calories-burned')
+    const remainingDiv = document.querySelector('#calories-remaining')
+
+    this._displayCaloriesLimit(limitDiv)
+    this._displayCaloriesTotal(totalDiv)
+    this._displayCaloriesConsumed(consumedDiv)
+    this._displayCaloriesBurned(burnedDiv)
+    this._displayCaloriesRemaining(remainingDiv)
+  }
+
+  get caloriesConsumed() {
+    return this._meals.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.calories,
+      0
+    )
+  }
+
+  get caloriesBurned() {
+    return this._workouts.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.calories,
+      0
+    )
+  }
+
+  get caloriesTotal() {
+    return this.caloriesConsumed - this.caloriesBurned
+  }
+
+  get caloriesRemaining() {
+    if (this.caloriesTotal === 0) {
+      return 0
+    }
+    return this._calorieLimit - this.caloriesTotal
+  }
+
+  setLimit(limit) {
+    this._calorieLimit = parseInt(limit)
+    this._renderStats()
+  }
+
+  resetDay() {
+    this._calorieLimit = 2000
+    this._totalCalories = 0
+    this._meals = []
+    this._workouts = []
+    this._renderStats()
+  }
+
+  loadItems() {
+    this._renderStats()
+    this._displayMeals()
+    this._displayWorkouts()
+  }
+
+  _displayMeals() {
+    const mealItems = document.querySelector('#meal-items')
+    if (mealItems) {
+      mealItems.innerHTML = ''
+      this._meals.forEach(({ id, name, calories }) => {
+        const div = document.createElement('div')
+        div.className = 'card my-2'
+        div.innerHTML = `
+          <div class="card-body">
+            <div class="d-flex align-items-center justify-content-between">
+              <h4 class="mx-1">${name}</h4>
+              <div
+                class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+              >
+                ${calories}
+              </div>
+              <button class="delete btn btn-danger btn-sm mx-2">
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+          </div>
+        `
+        mealItems.appendChild(div)
+      })
+    }
+  }
+
+  _displayWorkouts() {
+    const workoutItems = document.querySelector('#workout-items')
+    if (workoutItems) {
+      workoutItems.innerHTML = ''
+      this._workouts.forEach(({ name, calories }) => {
+        const div = document.createElement('div')
+        div.className = 'card my-2'
+        div.innerHTML = `
+          <div class="card-body">
+            <div class="d-flex align-items-center justify-content-between">
+              <h4 class="mx-1">${name}</h4>
+              <div
+                class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+              >
+                ${calories}
+              </div>
+              <button class="delete btn btn-danger btn-sm mx-2">
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+          </div>
+        `
+        workoutItems.appendChild(div)
+      })
+    }
+  }
 }
 
+// Meal class
 class Meal {
   constructor(name, calories) {
     this.id = Math.random().toString(16).slice(2)
@@ -32,46 +176,100 @@ class Workout {
     this.calories = calories
   }
 }
-// ------------------------------
 
-document.addEventListener('DOMContentLoaded', () => {
-  const tracker = new CalorieTracker()
+// App class
+class App {
+  constructor() {
+    // Initialize a CalorieTracker instance
+    const tracker = new CalorieTracker()
 
-  tracker.addMeal(new Meal('Breakfast', 500))
-  tracker.addMeal(new Meal('Lunch', 300))
-  tracker.addWorkout(new Workout('Run', 320))
-  tracker.addWorkout(new Workout('Push ups', 200))
+    // Add `DOMContentLoaded` event listener for document
+    document.addEventListener('DOMContentLoaded', () => {
+      tracker.addMeal(new Meal('Breakfast', 350))
+      tracker.addMeal(new Meal('Lunch', 620))
+      tracker.addWorkout(new Workout('Treadmill 1 Hour', 400))
+      tracker.addWorkout(new Workout('Bike 90 Minutes', 500))
+      tracker.loadItems()
+    })
 
-  const caloriesLimit = tracker._calorieLimit
-  const caloriesConsumed = tracker._meals.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.calories,
-    0
-  )
-  const caloriesBurned = tracker._workouts.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.calories,
-    0
-  )
-  const caloriesTotal = caloriesConsumed - caloriesBurned
-  const caloriesRemaining = caloriesLimit - caloriesTotal
+    // Set calories limit
+    this._setLimit(tracker)
 
-  const caloriesLimitDiv = document.querySelector('#calories-limit')
-  const caloriesTotalDiv = document.querySelector('#calories-total')
-  const caloriesConsumedDiv = document.querySelector('#calories-consumed')
-  const caloriesBurnedDiv = document.querySelector('#calories-burned')
-  const caloriesRemainingDiv = document.querySelector('#calories-remaining')
-  if (
-    caloriesLimitDiv &&
-    caloriesTotalDiv &&
-    caloriesConsumedDiv &&
-    caloriesBurnedDiv &&
-    caloriesRemainingDiv
-  ) {
-    caloriesLimitDiv.textContent = caloriesLimit
-    caloriesTotalDiv.textContent = caloriesConsumed - caloriesBurned
-    caloriesConsumedDiv.textContent = caloriesConsumed
-    caloriesBurnedDiv.textContent = caloriesBurned
-    caloriesRemainingDiv.textContent = caloriesRemaining
+    // Reset day
+    this._reset(tracker)
+
+    // Add new item
+    this._newItem(tracker)
   }
-  console.log(caloriesLimitDiv)
-})
 
+  _setLimit(tracker) {
+    // Add `submit` event listener for `limit-form`
+    const limitForm = document.querySelector('#limit-form')
+    if (limitForm) {
+      limitForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const limit = document.querySelector('#limit')
+        if (limit) {
+          const val = parseInt(limit.value)
+          if (!isNaN(val)) {
+            tracker.setLimit(val)
+          }
+          limit.value = ''
+        }
+      })
+    }
+  }
+
+  _reset(tracker) {
+    // Add `click` event listener for `reset button`
+    const resetBtn = document.querySelector('#reset')
+    if (resetBtn) {
+      resetBtn.addEventListener('click', () => {
+        tracker.resetDay()
+      })
+    }
+  }
+
+  _newItem(tracker) {
+    const mealForm = document.querySelector('#meal-form')
+    const workoutForm = document.querySelector('#workout-form')
+    if (mealForm && workoutForm) {
+      mealForm.addEventListener('submit', (e) =>
+        this._addNewItem(e, tracker, '#meal-name', '#meal-calories')
+      )
+      workoutForm.addEventListener('submit', (e) =>
+        this._addNewItem(
+          e,
+          tracker,
+          '#workout-name',
+          '#workout-calories',
+          false
+        )
+      )
+    }
+  }
+
+  _addNewItem(e, tracker, nameInpId, caloriesInpId, isMeal = true) {
+    e.preventDefault()
+    const nameInp = document.querySelector(nameInpId)
+    const caloriesInp = document.querySelector(caloriesInpId)
+    if (nameInp && caloriesInp) {
+      const name = nameInp.value.trim()
+      const calories = parseInt(caloriesInp.value)
+      if (name !== '' && !isNaN(calories)) {
+        if (isMeal) {
+          tracker.addMeal(new Meal(name, calories))
+        } else {
+          tracker.addWorkout(new Workout(name, calories))
+        }
+        tracker.loadItems()
+      }
+    }
+  }
+
+  _removeItem() {}
+}
+
+// ------------------------------------------
+// Run app
+new App()
